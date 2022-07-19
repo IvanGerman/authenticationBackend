904b6bdb56ec2f12aa6433e3f1252fde09ecabac
IvanGerman <ivgvi@mail.ru>
2022-07-19 17:47:32 +0700

    feat: create connection to mongodb

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const { MONGO_CONNECTION_STRING } = require('./common/config.js');
+
+
+mongoose.connect(MONGO_CONNECTION_STRING)
+  .then( () => { console.log('MongoDB connected!');})
+  .catch( (error) => { console.log(error);})
 
 const app = express();
 
